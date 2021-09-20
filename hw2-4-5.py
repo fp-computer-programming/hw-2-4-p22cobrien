24b5c39524a4b0e7a10cebc66b1fc6ad75b5006a
p22cobrien <p22cobrien@fairifeldprep.org>
2021-09-20 17:20:40 -0400

    add hw2-4-5.py

---
+++ hw2-4-5.py
@@ -0,0 +1,11 @@
+# Author: CMOB 9/20/2021
+
+a = 1
+b = 3
+c = -4
+
+solution1 = ((-b + (((b ** 2)- 4 * a * c) ** 0.5)) / (2 * a))
+
+solution2 = ((-b -(((b ** 2) - 4 * a* c) ** 0.5)) / (2 * a))
+
+print(solution1, solution2)
