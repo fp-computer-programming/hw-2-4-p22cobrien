bbb81e23dde12356b4d7936154dc2c6a899bfd6a
p22cobrien <p22cobrien@fairfieldprep.org>
2021-09-21 10:14:46 -0400

    fix 2-4-5

--- hw2-4-5.py
+++ hw2-4-5.py
@@ -4,8 +4,8 @@ a = 1
 b = 3
 c = -4
 
-solution1 = ((-b + (((b ** 2)- 4 * a * c) ** 0.5)) / (2 * a))
+solution1 = ((-b + (((b ** 2) - 4 * a * c) ** 0.5)) / (2 * a))
 
-solution2 = ((-b -(((b ** 2) - 4 * a* c) ** 0.5)) / (2 * a))
+solution2 = ((-b - (((b ** 2) - 4 * a * c) ** 0.5)) / (2 * a))
 
 print(solution1, solution2)
